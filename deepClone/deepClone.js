ec3171d648f880b7e40745e089f2dca95d63782f
donghyeonho <ehdgus1128@gmail.com>
2021-08-24 18:46:15 +0900

    [donghyeonho_20210824]     1. update '/deepClone'     - original annotations     - add new code

--- deepClone/deepClone.js
+++ deepClone/deepClone.js
@@ -3,18 +3,25 @@ function deepClone(source){
     throw 'Only Objects or Arrays are supported.'
   }
   
-  var target = source instanceof Array ? [] : {};
+  // var target = source instanceof Array ? [] : {};
   
-  for (let prop in source){
-    if ( source instanceof Object && !(source instanceof Array) && !(source.hasOwnProperty(prop)) ) {
-      continue;
-    }
+  // for (let prop in source){
+  //   if ( source instanceof Object && !(source instanceof Array) && !(source.hasOwnProperty(prop)) ) {
+  //     continue;
+  //   }
     
-    if ( source[prop] instanceof Object && !(source[prop] instanceof Date) && !(source[prop] instanceof String) )  {
-      target[prop] = deepClone(source[prop])
-    } else {
-      target[prop] = source[prop]
-    }
+  //   if ( source[prop] instanceof Object && !(source[prop] instanceof Date) && !(source[prop] instanceof String) )  {
+  //     target[prop] = deepClone(source[prop])
+  //   } else {
+  //     target[prop] = source[prop]
+  //   }
+  // }
+
+  var target;
+  target = Array.isArray(source) ? [] : {};
+   for (let key in source) {
+       let v = source[key];
+       target[key] = (typeof v === "object") ? deepClone(v) : v;
   }
 
   return target;
